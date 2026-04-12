c567e5644d821faec48c96ea2f22246c2084c89e
Michal <galusmichal@outlook.com>
2026-04-12 22:10:30 +0200

    first commit

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/DisciplineAppApplication.java
@@ -0,0 +1,13 @@
+package pl.disciplineapp.DisciplineApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciplineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciplineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/disciplineapp/DisciplineApp/DisciplineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.disciplineapp.DisciplineApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisciplineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

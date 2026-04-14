8ed108247dd487bf4499bbebebc23724187e3c75
Michal <galusmichal@outlook.com>
2026-04-14 06:58:58 +0200

    SCRUM-01 added findByEmail to UserRepository

--- src/main/java/pl/disciplineapp/DisciplineApp/repository/UserRepository.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/repository/UserRepository.java
@@ -3,5 +3,8 @@ package pl.disciplineapp.DisciplineApp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.disciplineapp.DisciplineApp.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }

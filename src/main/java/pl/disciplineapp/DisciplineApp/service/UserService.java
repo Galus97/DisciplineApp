2c189c1488d463f025c3f484151118a4e93b3ae6
Michal <galusmichal@outlook.com>
2026-04-15 09:10:53 +0200

    SCRUM-01 added UserService.java

--- src/main/java/pl/disciplineapp/DisciplineApp/service/UserService.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/service/UserService.java
@@ -2,8 +2,11 @@ package pl.disciplineapp.DisciplineApp.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.disciplineapp.DisciplineApp.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
 }

a3e7a705a625a850cc1b6591ea6371a4df01cd48
Michal <galusmichal@outlook.com>
2026-04-22 08:02:14 +0200

    SCRUM-01 added fromEntity to UserResponse

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/response/UserResponse.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/response/UserResponse.java
@@ -4,5 +4,14 @@ import pl.disciplineapp.DisciplineApp.entity.User;
 
 public record UserResponse(Long userId, String firstName, String lastName, String email, String password,
                            Boolean enabled, Boolean isSubscriber ) {
-
+    public static UserResponse fromEntity(User user) {
+        return new UserResponse(
+                user.getUserId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getEnabled(),
+                user.getIsSubscriber());
+    }
 }

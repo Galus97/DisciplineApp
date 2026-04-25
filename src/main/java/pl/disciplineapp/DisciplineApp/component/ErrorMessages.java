8b3f9c737e2ec8bdc4157d785c75cc201811a509
Michal <galusmichal@outlook.com>
2026-04-25 18:12:13 +0200

    SCRUM-01 added USER_REQUEST_IS_NULL to ErrorMessages

--- src/main/java/pl/disciplineapp/DisciplineApp/component/ErrorMessages.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/component/ErrorMessages.java
@@ -3,4 +3,5 @@ package pl.disciplineapp.DisciplineApp.component;
 public final class ErrorMessages {
     private final String USER_NOT_FOUND = "error.userNotFound";
     private final String INVALID_USER_ID = "error.invalidUserId";
+    private final String USER_REQUEST_IS_NULL = "error.userRequestIsNull";
 }

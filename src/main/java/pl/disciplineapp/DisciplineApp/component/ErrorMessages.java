9c438a493eb231d0e844e46037f7d22d029c5042
Michal <galusmichal@outlook.com>
2026-04-25 18:12:04 +0200

    SCRUM-01 added INVALID_USER_ID to ErrorMessages

--- src/main/java/pl/disciplineapp/DisciplineApp/component/ErrorMessages.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/component/ErrorMessages.java
@@ -2,4 +2,5 @@ package pl.disciplineapp.DisciplineApp.component;
 
 public final class ErrorMessages {
     private final String USER_NOT_FOUND = "error.userNotFound";
+    private final String INVALID_USER_ID = "error.invalidUserId";
 }

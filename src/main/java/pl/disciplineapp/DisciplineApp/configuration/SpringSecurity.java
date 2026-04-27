66be72620e6afcdbc31d118b35afdf5e8a931cfc
Michal <galusmichal@outlook.com>
2026-04-27 09:27:07 +0200

    SCRUM-01 added securityFilterChain to SpringSecurity

--- src/main/java/pl/disciplineapp/DisciplineApp/configuration/SpringSecurity.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/configuration/SpringSecurity.java
@@ -1,9 +1,24 @@
 package pl.disciplineapp.DisciplineApp.configuration;
 
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
+
+    public SecurityFilterChain securityFilterChain (HttpSecurity http)  throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize
+                .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+        );
+        http.
+                authorizeHttpRequests((request) -> request
+                        .requestMatchers("/", "/CSS/**", "/js/**", "/images/**", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
 }

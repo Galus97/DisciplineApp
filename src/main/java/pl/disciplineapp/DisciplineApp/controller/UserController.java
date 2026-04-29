f45ba7c4fe92c51de8a80bedd2adff477eb5011b
Michal <galusmichal@outlook.com>
2026-04-29 20:03:00 +0200

    SCRUM-01 added createUser to UserController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/UserController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/UserController.java
@@ -4,10 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.disciplineapp.DisciplineApp.dto.request.UserRequest;
 import pl.disciplineapp.DisciplineApp.dto.response.UserResponse;
+import pl.disciplineapp.DisciplineApp.exception.ValidationException;
 import pl.disciplineapp.DisciplineApp.service.UserService;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +26,14 @@ public class UserController {
         return ResponseEntity.ok(userService.getUserResponse(id));
     }
 
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody UserRequest userRequest) {
+        try {
+            UserResponse savedUser = userService.saveNewUser(userRequest);
+            return ResponseEntity.created(URI.create("/user/" + savedUser.userId())).body(savedUser);
+        } catch (ValidationException e) {
+            return ResponseEntity.badRequest().body(e.getValidationsErrors());
+        }
+    }
 }
 

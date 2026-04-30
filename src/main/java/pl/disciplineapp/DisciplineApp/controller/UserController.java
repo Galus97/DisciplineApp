124151e1b84547f2e04ae049e1b7f03ac2f14d4e
Michal <galusmichal@outlook.com>
2026-04-30 19:46:07 +0200

    SCRUM-01 added updateUser to UserController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/UserController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,11 @@ public class UserController {
         } catch (ValidationException e) {
             return ResponseEntity.badRequest().body(e.getValidationsErrors());
         }
+    }
+
+    @PutMapping
+    public ResponseEntity<UserResponse> updateUser(@RequestBody UserRequest userRequest) {
+        return ResponseEntity.ok(userService.updateUser(userRequest));
     }
 }
 

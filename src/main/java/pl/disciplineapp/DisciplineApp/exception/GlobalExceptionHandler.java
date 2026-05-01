48f37418e24009118e7a8dc1e307927f956467f7
Michal <galusmichal@outlook.com>
2026-05-01 09:30:57 +0200

    SCRUM-01 added GlobalExceptionHandler

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package pl.disciplineapp.DisciplineApp.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}

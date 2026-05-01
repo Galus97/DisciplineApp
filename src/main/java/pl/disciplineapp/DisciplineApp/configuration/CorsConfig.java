40a93bb45380d789025de00758052fb8123f1344
Michal <galusmichal@outlook.com>
2026-05-01 09:26:40 +0200

    SCRUM-01 added CorsConfig.java

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/configuration/CorsConfig.java
@@ -0,0 +1,6 @@
+package pl.disciplineapp.DisciplineApp.configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+}

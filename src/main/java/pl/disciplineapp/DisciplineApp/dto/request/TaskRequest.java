a5ad3d07d2cbf2559ae5175d937e6c2dffbeb843
Michal <galusmichal@outlook.com>
2026-05-13 23:11:14 +0200

    SCRUM-01 added fields to TaskRequest

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/request/TaskRequest.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/request/TaskRequest.java
@@ -3,7 +3,16 @@ package pl.disciplineapp.DisciplineApp.dto.request;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class TaskRequest {
+    private Long taskId;
+    private String taskName;
+    private String description;
+    private boolean completed;
+    private LocalDateTime createdAt;
+    private LocalDateTime completedAt;
+    private LocalDateTime deadline;
 }

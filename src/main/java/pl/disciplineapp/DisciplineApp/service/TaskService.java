19fe6a34f99d420db37f3bd6bfdb4d263e74b33b
Michal <galusmichal@outlook.com>
2026-05-14 18:18:55 +0200

    SCRUM-01 added getTaskResponse to TaskService

--- src/main/java/pl/disciplineapp/DisciplineApp/service/TaskService.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/service/TaskService.java
@@ -3,6 +3,7 @@ package pl.disciplineapp.DisciplineApp.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.disciplineapp.DisciplineApp.component.MessageService;
+import pl.disciplineapp.DisciplineApp.dto.response.TaskResponse;
 import pl.disciplineapp.DisciplineApp.repository.TaskRepository;
 
 @Service
@@ -11,5 +12,7 @@ public class TaskService {
     private final TaskRepository taskRepository;
     private final MessageService messageService;
 
-
+    public TaskResponse getTaskResponse(Long taskId) {
+        return TaskResponse.fromEntity(taskRepository.findById(taskId).orElseThrow());
+    }
 }

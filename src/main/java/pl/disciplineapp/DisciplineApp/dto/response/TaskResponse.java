c1ee8944a7f320c6ccfd0d7d372f60a6cd378904
Michal <galusmichal@outlook.com>
2026-05-14 18:17:03 +0200

    SCRUM-01 added fromEntity to TaskResponse

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/response/TaskResponse.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/response/TaskResponse.java
@@ -6,5 +6,16 @@ import java.time.LocalDateTime;
 
 public record TaskResponse(Long taskId, String taskName, String description, boolean completed,
                            LocalDateTime createdAt, LocalDateTime completedAt,  LocalDateTime deadline) {
-    
+
+    public static TaskResponse fromEntity(Task task) {
+        return new TaskResponse(
+                task.getTaskId(),
+                task.getTaskName(),
+                task.getDescription(),
+                task.isCompleted(),
+                task.getCreatedAt(),
+                task.getCompletedAt(),
+                task.getDeadline()
+        );
+    }
 }

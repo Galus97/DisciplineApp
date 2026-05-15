4136046a33fbca72ee78ebd9c50758f9e64408d4
Michal <galusmichal@outlook.com>
2026-05-15 14:11:29 +0200

    SCRUM-01 added throwIfIdIsNotValid to TaskService

--- src/main/java/pl/disciplineapp/DisciplineApp/service/TaskService.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/service/TaskService.java
@@ -2,6 +2,7 @@ package pl.disciplineapp.DisciplineApp.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.disciplineapp.DisciplineApp.component.ErrorMessages;
 import pl.disciplineapp.DisciplineApp.component.MessageService;
 import pl.disciplineapp.DisciplineApp.dto.response.TaskResponse;
 import pl.disciplineapp.DisciplineApp.repository.TaskRepository;
@@ -13,6 +14,14 @@ public class TaskService {
     private final MessageService messageService;
 
     public TaskResponse getTaskResponse(Long taskId) {
+        throwIfIdIsNotValid(taskId);
         return TaskResponse.fromEntity(taskRepository.findById(taskId).orElseThrow());
+    }
+
+
+    private void throwIfIdIsNotValid(Long taskId) {
+        if (taskId == null || taskId < 1) {
+            throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVALID_TASK_ID));
+        }
     }
 }

a5f5c7b977089d3ea1666c5b365e4f56e8b856e9
Michal <galusmichal@outlook.com>
2026-05-20 16:11:39 +0200

    SCRUM-01 added showTask to TaskController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/TaskController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/TaskController.java
@@ -1,8 +1,12 @@
 package pl.disciplineapp.DisciplineApp.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.disciplineapp.DisciplineApp.dto.response.TaskResponse;
 import pl.disciplineapp.DisciplineApp.service.TaskService;
 
 @RestController
@@ -10,4 +14,9 @@ import pl.disciplineapp.DisciplineApp.service.TaskService;
 @RequestMapping("/task")
 public class TaskController {
     private final TaskService taskService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponse> showTask(@PathVariable Long id) {
+        return ResponseEntity.ok(taskService.getTaskResponse(id));
+    }
 }

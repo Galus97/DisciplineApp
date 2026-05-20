d7f981239f430140526c6a3a18fe5652a7a7046f
Michal <galusmichal@outlook.com>
2026-05-20 16:15:19 +0200

    SCRUM-01 added createTask to TaskController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/TaskController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/TaskController.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.disciplineapp.DisciplineApp.dto.request.TaskRequest;
 import pl.disciplineapp.DisciplineApp.dto.response.TaskResponse;
 import pl.disciplineapp.DisciplineApp.service.TaskService;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +23,11 @@ public class TaskController {
     @GetMapping("/{id}")
     public ResponseEntity<TaskResponse> showTask(@PathVariable Long id) {
         return ResponseEntity.ok(taskService.getTaskResponse(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponse> createTask(@RequestBody TaskRequest taskRequest) {
+        TaskResponse savedTask = taskService.saveTask(taskRequest);
+        return ResponseEntity.created(URI.create("/task/" + savedTask.taskId())).body(savedTask);
     }
 }

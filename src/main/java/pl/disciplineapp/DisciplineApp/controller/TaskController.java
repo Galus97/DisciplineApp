5fc2511852e284e81e5d3d3ccf48dd25706dac79
Michal <galusmichal@outlook.com>
2026-05-21 12:25:34 +0200

    SCRUM-01 added deleteTask to TaskController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/TaskController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/TaskController.java
@@ -2,6 +2,7 @@ package pl.disciplineapp.DisciplineApp.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +36,11 @@ public class TaskController {
     @PutMapping
     public ResponseEntity<TaskResponse> updateTask(@RequestBody TaskRequest taskRequest) {
         return ResponseEntity.ok(taskService.updateTask(taskRequest));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void>  deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
     }
 }

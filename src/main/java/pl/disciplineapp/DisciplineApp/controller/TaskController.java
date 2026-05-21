bba6cb0abc116c95538735bf9eaa5d4dda848f24
Michal <galusmichal@outlook.com>
2026-05-21 12:24:06 +0200

    SCRUM-01 added updateTask to TaskController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/TaskController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/TaskController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,10 @@ public class TaskController {
     public ResponseEntity<TaskResponse> createTask(@RequestBody TaskRequest taskRequest) {
         TaskResponse savedTask = taskService.saveTask(taskRequest);
         return ResponseEntity.created(URI.create("/task/" + savedTask.taskId())).body(savedTask);
+    }
+
+    @PutMapping
+    public ResponseEntity<TaskResponse> updateTask(@RequestBody TaskRequest taskRequest) {
+        return ResponseEntity.ok(taskService.updateTask(taskRequest));
     }
 }

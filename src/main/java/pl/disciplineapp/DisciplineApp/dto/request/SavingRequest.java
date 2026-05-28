0fb982bd0e4077e57f2873e8741c3295a8dd2cc3
michalgalus <galusmichal@outlook.com>
2026-05-28 19:21:15 +0200

    SCRUM-01 added fields to SavingRequest

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/request/SavingRequest.java
@@ -0,0 +1,14 @@
+package pl.disciplineapp.DisciplineApp.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SavingRequest {
+    private Long savingId;
+    private String savingType;
+    private Float TotalValue;
+    private Float quantity;
+    private Float unitPrice;
+}

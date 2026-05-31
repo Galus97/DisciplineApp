f8c16e9a28393af044522f6917bd623ff83f6237
michalgalus <galusmichal@outlook.com>
2026-05-31 19:11:28 +0200

    SCRUM-01 added getSavingOrThrowIfNotExist to SavingsService

--- src/main/java/pl/disciplineapp/DisciplineApp/service/SavingsService.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/service/SavingsService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.disciplineapp.DisciplineApp.component.ErrorMessages;
 import pl.disciplineapp.DisciplineApp.component.MessageService;
+import pl.disciplineapp.DisciplineApp.entity.Saving;
+import pl.disciplineapp.DisciplineApp.exception.SavingNotFoundException;
 import pl.disciplineapp.DisciplineApp.repository.SavingRepository;
 
 @Service
@@ -11,6 +13,11 @@ import pl.disciplineapp.DisciplineApp.repository.SavingRepository;
 public class SavingsService {
     private final SavingRepository savingRepository;
     private final MessageService messageService;
+
+    private Saving getSavingOrThrowIfNotExist(Long savingId) {
+        return savingRepository.findById(savingId).orElseThrow(
+                () -> new SavingNotFoundException(messageService.getMessage(ErrorMessages.SAVING_NOT_FOUND)));
+    }
 
     private void throwIfIdIsNotValid(Long savingId) {
         if (savingId == null || savingId <= 0) {

ed98113df6efbc8ca592c8fcbe219ecbfaca98e7
michalgalus <galusmichal@outlook.com>
2026-06-08 21:49:16 +0200

    SCRUM-01 added saveSaving to SavingController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/SavingController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/SavingController.java
@@ -2,12 +2,12 @@ package pl.disciplineapp.DisciplineApp.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.disciplineapp.DisciplineApp.dto.request.SavingRequest;
 import pl.disciplineapp.DisciplineApp.dto.response.SavingResponse;
 import pl.disciplineapp.DisciplineApp.service.SavingService;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +18,11 @@ public class SavingController {
     @GetMapping("/id")
     public ResponseEntity<SavingResponse> showSaving(@PathVariable Long id) {
         return ResponseEntity.ok(savingService.getSavingResponse(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<SavingResponse> saveSaving(@RequestBody SavingRequest savingRequest) {
+        SavingResponse savedSaving = savingService.saveSaving(savingRequest);
+        return ResponseEntity.created(URI.create("/saving/" + savedSaving.savingId())).body(savedSaving);
     }
 }

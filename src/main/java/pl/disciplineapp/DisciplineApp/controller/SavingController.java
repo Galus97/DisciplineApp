635bebf099ce5fa655829d110e8fee93efffd07d
michalgalus <galusmichal@outlook.com>
2026-06-08 21:45:22 +0200

    SCRUM-01 added showSaving to SavingController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/SavingController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/SavingController.java
@@ -1,13 +1,22 @@
 package pl.disciplineapp.DisciplineApp.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.disciplineapp.DisciplineApp.dto.response.SavingResponse;
 import pl.disciplineapp.DisciplineApp.service.SavingService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/savong")
+@RequestMapping("/saving")
 public class SavingController {
     private final SavingService savingService;
+
+    @GetMapping("/id")
+    public ResponseEntity<SavingResponse> showSaving(@PathVariable Long id) {
+        return ResponseEntity.ok(savingService.getSavingResponse(id));
+    }
 }

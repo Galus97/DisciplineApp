31195a9c9167609a9be89a4b5ca0c2e652a79478
michalgalus <galusmichal@outlook.com>
2026-06-13 13:32:42 +0200

    SCRUM-01 added fields to ExpenseResponse

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/response/ExpenseResponse.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/response/ExpenseResponse.java
@@ -1,4 +1,15 @@
 package pl.disciplineapp.DisciplineApp.dto.response;
 
-public record ExpenseResponse() {
+import pl.disciplineapp.DisciplineApp.entity.Expense;
+
+public record ExpenseResponse(Long expenseId, String expenseType, Float totalValue, Float quantity, Float unitPrice) {
+    public static ExpenseResponse fromEntity(Expense expense) {
+        return new ExpenseResponse(
+                expense.getExpenseId(),
+                expense.getExpenseType(),
+                expense.getTotalValue(),
+                expense.getQuantity(),
+                expense.getUnitPrice()
+        );
+    }
 }

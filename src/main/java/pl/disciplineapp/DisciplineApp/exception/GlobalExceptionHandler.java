7c89a89a7f8e72ce828d560859e4aa58120a516e
michalgalus <galusmichal@outlook.com>
2026-06-14 04:48:28 +0200

    SCRUM-01 added handleSavingNotFoundException to GlobalExceptionHandler

--- src/main/java/pl/disciplineapp/DisciplineApp/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package pl.disciplineapp.DisciplineApp.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pl.disciplineapp.DisciplineApp.component.ErrorMessages;
 
@@ -10,6 +11,11 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SavingNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleSavingNotFoundException(SavingNotFoundException e) {
+        return getMapResponseEntity(e);
+    }
 
     private static ResponseEntity<Map<String, String>> getMapResponseEntity(RuntimeException exception) {
         Map<String, String> response = new HashMap<>();

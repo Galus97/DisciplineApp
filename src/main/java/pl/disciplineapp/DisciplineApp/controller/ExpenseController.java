606d2759f6dd49191a86574a3233ea26a96fe6b1
michalgalus <galusmichal@outlook.com>
2026-06-19 16:54:30 +0200

    SCRUM-01 added showExpense to ExpenseController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/ExpenseController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/ExpenseController.java
@@ -1,11 +1,22 @@
 package pl.disciplineapp.DisciplineApp.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.disciplineapp.DisciplineApp.dto.response.ExpenseResponse;
+import pl.disciplineapp.DisciplineApp.service.ExpenseService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/expense")
 public class ExpenseController {
+    private final ExpenseService expenseService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ExpenseResponse> showExpense(@PathVariable Long id) {
+        return ResponseEntity.ok(expenseService.getExpenseResponse(id));
+    }
 }

399694ff76840c9ef21f43f9e14e478ef8f0c548
michalgalus <galusmichal@outlook.com>
2026-06-21 13:17:41 +0200

    SCRUM-01 added fields to InvestmentResponse

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/request/InvestmentResponse.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/request/InvestmentResponse.java
@@ -1,4 +1,14 @@
 package pl.disciplineapp.DisciplineApp.dto.request;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class InvestmentResponse {
+    private Long investmentId;
+    private String investmentType;
+    private Float totalValue;
+    private Float quantity;
+    private Float unitPrice;
 }

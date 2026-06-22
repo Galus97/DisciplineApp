0259028599266c0b7001e85d1603090adf67531a
michalgalus <galusmichal@outlook.com>
2026-06-22 17:00:24 +0200

    SCRUM-01 added fromEntity to InvestmentRequest

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/response/InvestmentRequest.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/response/InvestmentRequest.java
@@ -1,4 +1,17 @@
 package pl.disciplineapp.DisciplineApp.dto.response;
 
-public record InvestmentRequest() {
+import pl.disciplineapp.DisciplineApp.entity.Investment;
+
+public record InvestmentRequest(Long investmentId, String investmentType, Float totalValue,
+                                Float quantity, Float unitPrice) {
+
+    public static InvestmentRequest fromEntity(Investment investment) {
+        return new InvestmentRequest(
+                investment.getInvestmentId(),
+                investment.getInvestmentType(),
+                investment.getTotalValue(),
+                investment.getQuantity(),
+                investment.getUnitPrice()
+        );
+    }
 }

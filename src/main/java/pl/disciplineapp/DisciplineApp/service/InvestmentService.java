020a2e67f3235442f7dc600468cd0f27b3bc5172
michalgalus <galusmichal@outlook.com>
2026-06-23 10:32:53 +0200

    SCRUM-01 added throwIfIdIsNotValid to InvestmentService

--- src/main/java/pl/disciplineapp/DisciplineApp/service/InvestmentService.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/service/InvestmentService.java
@@ -2,10 +2,19 @@ package pl.disciplineapp.DisciplineApp.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.disciplineapp.DisciplineApp.component.ErrorMessages;
+import pl.disciplineapp.DisciplineApp.component.MessageService;
 import pl.disciplineapp.DisciplineApp.repository.InvestmentRepository;
 
 @Service
 @RequiredArgsConstructor
 public class InvestmentService {
     private final InvestmentRepository investmentRepository;
+    private final MessageService messageService;
+
+    private void throwIfIdIsNotValid(Long id) {
+        if (id == null || id <= 0) {
+           throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVALID_INVESTMENT_ID));
+        }
+    }
 }

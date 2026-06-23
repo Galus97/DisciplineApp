086cef6c90275c78fe934293cd616120a90574d4
michalgalus <galusmichal@outlook.com>
2026-06-23 10:35:18 +0200

    SCRUM-01 added throwIfRequestIsNull to InvestmentService

--- src/main/java/pl/disciplineapp/DisciplineApp/service/InvestmentService.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/service/InvestmentService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.disciplineapp.DisciplineApp.component.ErrorMessages;
 import pl.disciplineapp.DisciplineApp.component.MessageService;
+import pl.disciplineapp.DisciplineApp.dto.response.InvestmentRequest;
 import pl.disciplineapp.DisciplineApp.repository.InvestmentRepository;
 
 @Service
@@ -11,6 +12,12 @@ import pl.disciplineapp.DisciplineApp.repository.InvestmentRepository;
 public class InvestmentService {
     private final InvestmentRepository investmentRepository;
     private final MessageService messageService;
+
+    private void throwIfRequestIsNull(InvestmentRequest investmentRequest) {
+        if (investmentRequest == null) {
+           throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVESTMENT_REQUEST_IS_NULL));
+        }
+    }
 
     private void throwIfIdIsNotValid(Long id) {
         if (id == null || id <= 0) {

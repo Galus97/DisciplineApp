1a5caf580d67e5944cc86999afae1c2a6af7352b
michalgalus <galusmichal@outlook.com>
2026-06-27 14:05:24 +0200

    SCRUM-01 added saveInvestment to InvestmentController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/InvestmentController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/InvestmentController.java
@@ -2,12 +2,12 @@ package pl.disciplineapp.DisciplineApp.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.disciplineapp.DisciplineApp.dto.request.InvestmentRequest;
 import pl.disciplineapp.DisciplineApp.dto.response.InvestmentResponse;
 import pl.disciplineapp.DisciplineApp.service.InvestmentService;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +18,12 @@ public class InvestmentController {
     @GetMapping("/{id}")
     public ResponseEntity<InvestmentResponse> showInvestment(@PathVariable Long id) {
         return ResponseEntity.ok(investmentService.getInvestment(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<InvestmentResponse> saveInvestment(@RequestBody InvestmentRequest investmentRequest) {
+        InvestmentResponse savedInvestment = investmentService.saveInvestment(investmentRequest);
+        return ResponseEntity.created(URI.create("/investment/" + savedInvestment.investmentId()))
+                .body(savedInvestment);
     }
 }

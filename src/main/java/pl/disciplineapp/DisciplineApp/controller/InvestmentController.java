4aee894a05c4eb78e86900ddc9e4f48783af623f
michalgalus <galusmichal@outlook.com>
2026-06-27 14:01:39 +0200

    SCRUM-01 added showInvestment to InvestmentControlle

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/InvestmentController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/InvestmentController.java
@@ -1,8 +1,12 @@
 package pl.disciplineapp.DisciplineApp.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.disciplineapp.DisciplineApp.dto.response.InvestmentResponse;
 import pl.disciplineapp.DisciplineApp.service.InvestmentService;
 
 @RestController
@@ -10,4 +14,9 @@ import pl.disciplineapp.DisciplineApp.service.InvestmentService;
 @RequestMapping("/investment")
 public class InvestmentController {
     private final InvestmentService investmentService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<InvestmentResponse> showInvestment(@PathVariable Long id) {
+        return ResponseEntity.ok(investmentService.getInvestment(id));
+    }
 }

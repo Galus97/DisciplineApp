576cc75e52ca3090e3a3e7c7332dec89f225bbaa
michalgalus <galusmichal@outlook.com>
2026-06-29 18:13:37 +0200

    SCRUM-01 updated ValidEntity

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/service/valid/ValidEntityService.java
@@ -0,0 +1,29 @@
+package pl.disciplineapp.DisciplineApp.service.valid;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.disciplineapp.DisciplineApp.component.ErrorMessages;
+import pl.disciplineapp.DisciplineApp.component.MessageService;
+import pl.disciplineapp.DisciplineApp.repository.valid.ValidEntity;
+
+@Service
+@RequiredArgsConstructor
+public class ValidEntityService implements ValidEntity {
+    private final MessageService messageService;
+    @Override
+    public void throwIfIdIsNotValid(Long id, String errorMessage) {
+        if (id == null || id < 1) {
+            throw new IllegalArgumentException(messageService.getMessage(errorMessage));
+        }
+    }
+
+    @Override
+    public void throwIfRequestIsNull(Object request) {
+
+    }
+
+    @Override
+    public Object getEntityOrThrowIfNotExist(Long id) {
+        return null;
+    }
+}

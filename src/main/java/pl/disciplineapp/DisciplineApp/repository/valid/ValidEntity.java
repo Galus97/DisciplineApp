3bd2f5c404dd78f2487e06679b6645e350b39354
michalgalus <galusmichal@outlook.com>
2026-06-29 18:15:31 +0200

    SCRUM-01 updated throwIfRequestIsNull in ValidEntity

--- src/main/java/pl/disciplineapp/DisciplineApp/repository/valid/ValidEntity.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/repository/valid/ValidEntity.java
@@ -2,6 +2,6 @@ package pl.disciplineapp.DisciplineApp.repository.valid;
 
 public interface ValidEntity {
     void throwIfIdIsNotValid (Long id, String errorMessage);
-    void throwIfRequestIsNull(Object request);
+    void throwIfRequestIsNull(Object request, String errorMessage);
     Object getEntityOrThrowIfNotExist(Long id);
 }

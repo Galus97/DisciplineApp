4db9650ce81c6bbf575d54b4b2aac27d061149c6
michalgalus <galusmichal@outlook.com>
2026-06-29 18:14:07 +0200

    SCRUM-01 added ValidEntityService.java

--- src/main/java/pl/disciplineapp/DisciplineApp/repository/valid/ValidEntity.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/repository/valid/ValidEntity.java
@@ -1,7 +1,7 @@
 package pl.disciplineapp.DisciplineApp.repository.valid;
 
 public interface ValidEntity {
-    void throwIfIdIsNotValid (Long id);
+    void throwIfIdIsNotValid (Long id, String errorMessage);
     void throwIfRequestIsNull(Object request);
     Object getEntityOrThrowIfNotExist(Long id);
 }

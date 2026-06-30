360fd0a57fca8dc0d3491263f605d5ce5f94f3d9
michalgalus <galusmichal@outlook.com>
2026-06-30 21:08:02 +0200

    SCRUM-01 deleted ValidEntity.java

--- src/main/java/pl/disciplineapp/DisciplineApp/repository/valid/ValidEntity.java
+++
@@ -1,6 +0,0 @@
-package pl.disciplineapp.DisciplineApp.repository.valid;
-
-public interface ValidEntity {
-    void throwIfIdIsNotValid (Long id, String errorMessage);
-    void throwIfRequestIsNull(Object request, String errorMessage);
-}

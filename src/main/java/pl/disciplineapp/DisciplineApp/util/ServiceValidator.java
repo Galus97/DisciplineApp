b304b71ec1ceff2612fcca41c5fb323acc6d6131
michalgalus <galusmichal@outlook.com>
2026-06-30 21:17:02 +0200

    SCRUM-01 added throwIfRequestIsNull to ServiceValidator

--- src/main/java/pl/disciplineapp/DisciplineApp/util/ServiceValidator.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/util/ServiceValidator.java
@@ -1,11 +1,17 @@
 package pl.disciplineapp.DisciplineApp.util;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import pl.disciplineapp.DisciplineApp.component.ErrorMessages;
 import pl.disciplineapp.DisciplineApp.component.MessageService;
-import pl.disciplineapp.DisciplineApp.dto.request.TaskRequest;
 
 @Component
+@RequiredArgsConstructor
 public class ServiceValidator {
-    
+    private final MessageService messageService;
+
+    public void throwIfRequestIsNull(Object request, String errorMessageKey) {
+        if (request == null) {
+            throw new IllegalArgumentException(messageService.getMessage(errorMessageKey));
+        }
+    }
 }

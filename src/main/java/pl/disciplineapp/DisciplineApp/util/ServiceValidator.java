129df2d6442497f158bba7e178a3d121903ad010
michalgalus <galusmichal@outlook.com>
2026-06-30 21:09:12 +0200

    SCRUM-01 added ServiceValidator.java

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/util/ServiceValidator.java
@@ -0,0 +1,11 @@
+package pl.disciplineapp.DisciplineApp.util;
+
+import org.springframework.stereotype.Component;
+import pl.disciplineapp.DisciplineApp.component.ErrorMessages;
+import pl.disciplineapp.DisciplineApp.component.MessageService;
+import pl.disciplineapp.DisciplineApp.dto.request.TaskRequest;
+
+@Component
+public class ServiceValidator {
+    
+}

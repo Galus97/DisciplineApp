785520a163a9e7015d33184487420f9014c241fa
michalgalus <galusmichal@outlook.com>
2026-07-08 11:08:21 +0200

    SCRUM-01 added RegisterValidator.java

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/util/RegisterValidator.java
@@ -0,0 +1,9 @@
+package pl.disciplineapp.DisciplineApp.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RegisterValidator {
+}

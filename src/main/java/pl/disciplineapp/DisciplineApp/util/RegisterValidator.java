73f22992cbe512828aae8367396bfdc774cf5b14
michalgalus <galusmichal@outlook.com>
2026-07-08 11:14:36 +0200

    SCRUM-01 added validateErrors to  RegisterValidator

--- src/main/java/pl/disciplineapp/DisciplineApp/util/RegisterValidator.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/util/RegisterValidator.java
@@ -2,8 +2,29 @@ package pl.disciplineapp.DisciplineApp.util;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import pl.disciplineapp.DisciplineApp.component.ErrorMessages;
+import pl.disciplineapp.DisciplineApp.component.MessageService;
+import pl.disciplineapp.DisciplineApp.entity.User;
+import pl.disciplineapp.DisciplineApp.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class RegisterValidator {
+    private final UserRepository userRepository;
+    private final MessageService messageService;
+
+    public List<String> validateErrors(User user) {
+        List<String> errors = new ArrayList<>();
+
+        Optional<User> ifUserExistByEmail = userRepository.findByEmail(user.getEmail());
+        if (ifUserExistByEmail.isEmpty()) {
+            errors.add(messageService.getMessage(ErrorMessages.EMAIL_IS_ALREADY_USED));
+        }
+
+        return errors;
+    }
 }

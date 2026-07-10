09ffecd49ea91f2399565c8863e91da4bb173446
michalgalus <galusmichal@outlook.com>
2026-07-10 17:15:11 +0200

    SCRUM-01 added serviceValidator to InvestmentService

--- src/main/java/pl/disciplineapp/DisciplineApp/service/InvestmentService.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/service/InvestmentService.java
@@ -9,40 +9,42 @@ import pl.disciplineapp.DisciplineApp.dto.response.InvestmentResponse;
 import pl.disciplineapp.DisciplineApp.entity.Investment;
 import pl.disciplineapp.DisciplineApp.exception.InvestmentNotFoundException;
 import pl.disciplineapp.DisciplineApp.repository.InvestmentRepository;
-
-import java.util.Optional;
+import pl.disciplineapp.DisciplineApp.util.ServiceValidator;
 
 @Service
 @RequiredArgsConstructor
 public class InvestmentService {
     private final InvestmentRepository investmentRepository;
     private final MessageService messageService;
+    private final ServiceValidator serviceValidator;
 
-    public InvestmentResponse getInvestment(Long id) {
-        throwIfIdIsNotValid(id);
-        return InvestmentResponse.fromEntity(getInvestmentOrThrowIfNotExist(id));
+    public InvestmentResponse getInvestment(Long investmentId) {
+        serviceValidator.throwIfIdIsNotValid(investmentId, ErrorMessages.INVALID_INVESTMENT_ID);
+        return InvestmentResponse.fromEntity(getInvestmentOrThrowIfNotExist(investmentId));
     }
 
     public InvestmentResponse saveInvestment(InvestmentRequest investmentRequest) {
-        throwIfRequestIsNull(investmentRequest);
+        serviceValidator.throwIfRequestIsNull(investmentRequest, ErrorMessages.INVESTMENT_REQUEST_IS_NULL);
         return InvestmentResponse.fromEntity(investmentRepository.save(buildInvestment(investmentRequest)));
     }
 
     public InvestmentResponse updateInvestment(InvestmentRequest investmentRequest) {
-        throwIfRequestIsNull(investmentRequest);
+        serviceValidator.throwIfRequestIsNull(investmentRequest, ErrorMessages.INVESTMENT_REQUEST_IS_NULL);
+        serviceValidator.throwIfIdIsNotValid(investmentRequest.getInvestmentId(), ErrorMessages.INVALID_INVESTMENT_ID);
 
         Investment existingInvestment = getInvestmentOrThrowIfNotExist(investmentRequest.getInvestmentId());
         existingInvestment.setInvestmentType(investmentRequest.getInvestmentType());
         existingInvestment.setTotalValue(investmentRequest.getTotalValue());
         existingInvestment.setQuantity(investmentRequest.getQuantity());
         existingInvestment.setUnitPrice(investmentRequest.getUnitPrice());
+        existingInvestment.setUser(investmentRequest.getUser());
 
         return InvestmentResponse.fromEntity(investmentRepository.save(existingInvestment));
     }
 
-    public void deleteInvestment(Long id) {
-        throwIfIdIsNotValid(id);
-        investmentRepository.delete(getInvestmentOrThrowIfNotExist(id));
+    public void deleteInvestment(Long investmentId) {
+        serviceValidator.throwIfIdIsNotValid(investmentId, ErrorMessages.INVALID_INVESTMENT_ID);
+        investmentRepository.delete(getInvestmentOrThrowIfNotExist(investmentId));
     }
 
     private Investment buildInvestment(InvestmentRequest investmentRequest) {
@@ -51,23 +53,12 @@ public class InvestmentService {
                 .totalValue(investmentRequest.getTotalValue())
                 .quantity(investmentRequest.getQuantity())
                 .unitPrice(investmentRequest.getUnitPrice())
+                .user(investmentRequest.getUser())
                 .build();
     }
 
     private Investment getInvestmentOrThrowIfNotExist(Long id) {
         return investmentRepository.findById(id).orElseThrow(
                 () -> new InvestmentNotFoundException(messageService.getMessage(ErrorMessages.INVESTMENT_NOT_FOUND)));
-    }
-
-    private void throwIfRequestIsNull(InvestmentRequest investmentRequest) {
-        if (investmentRequest == null) {
-           throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVESTMENT_REQUEST_IS_NULL));
-        }
-    }
-
-    private void throwIfIdIsNotValid(Long id) {
-        if (id == null || id <= 0) {
-           throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVALID_INVESTMENT_ID));
-        }
     }
 }

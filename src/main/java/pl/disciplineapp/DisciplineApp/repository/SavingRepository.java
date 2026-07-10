c8e841c70af70996e49d72fb8bb8a7319537b60c
michalgalus <galusmichal@outlook.com>
2026-07-10 16:43:15 +0200

    SCRUM-01 added findAllByUser_UserId to SavingRepository

--- src/main/java/pl/disciplineapp/DisciplineApp/repository/SavingRepository.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/repository/SavingRepository.java
@@ -3,5 +3,9 @@ package pl.disciplineapp.DisciplineApp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.disciplineapp.DisciplineApp.entity.Saving;
 
+import java.util.List;
+
 public interface SavingRepository extends JpaRepository<Saving, Long> {
+
+    List<Saving> findAllByUser_UserId(Long userId);
 }

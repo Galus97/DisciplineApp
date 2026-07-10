aceaaacfd31a90b17412e8bc113e15c00ce77fca
michalgalus <galusmichal@outlook.com>
2026-07-10 17:37:39 +0200

    SCRUM-01 added fromEntityList to SavingResponse

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/response/SavingResponse.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/response/SavingResponse.java
@@ -3,6 +3,9 @@ package pl.disciplineapp.DisciplineApp.dto.response;
 import pl.disciplineapp.DisciplineApp.entity.Saving;
 import pl.disciplineapp.DisciplineApp.entity.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public record SavingResponse(Long savingId, String savingType, Float TotalValue,
                              Float quantity, Float unitPrice, User user) {
     public static SavingResponse fromEntity(Saving saving) {
@@ -14,5 +17,16 @@ public record SavingResponse(Long savingId, String savingType, Float TotalValue,
                 saving.getUnitPrice(),
                 saving.getUser()
         );
+    }
+
+    public static List<SavingResponse> fromEntityList(List<Saving> savingList) {
+        List<SavingResponse> savingResponseList = new ArrayList<>();
+
+        if (!savingList.isEmpty()) {
+            for (Saving saving : savingList) {
+                savingResponseList.add(fromEntity(saving));
+            }
+        }
+        return savingResponseList;
     }
 }

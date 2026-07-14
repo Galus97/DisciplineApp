5a3a377eb5caa58a6952d0fbb7669c412560e279
michalgalus <galusmichal@outlook.com>
2026-07-14 12:30:30 +0200

    SCRUM-01 added findAllByUserIdAndCreatedAtBetween to ExpenseRepository

--- src/main/java/pl/disciplineapp/DisciplineApp/repository/ExpenseRepository.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/repository/ExpenseRepository.java
@@ -1,11 +1,20 @@
 package pl.disciplineapp.DisciplineApp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.disciplineapp.DisciplineApp.entity.Expense;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
     List<Expense> findAllByUser_UserId(Long userId);
+
+    @Query("SELECT e FROM Expense e WHERE e.user.userId = :userId AND e.createdAt BETWEEN :from AND :to")
+    List<Expense> findAllByUserIdAndCreatedAtBetween(
+            @Param("userId") Long userId,
+            @Param("from") LocalDateTime from,
+            @Param("to") LocalDateTime to);
 }

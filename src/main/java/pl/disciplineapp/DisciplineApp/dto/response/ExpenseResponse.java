3de4eb18c81840935298aac6dfc17e64c200cd98
michalgalus <galusmichal@outlook.com>
2026-07-27 17:24:55 +0200

    SCRUM-01 changed fromEntityList in  ExpenseResponse

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/response/ExpenseResponse.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/response/ExpenseResponse.java
@@ -5,6 +5,7 @@ import pl.disciplineapp.DisciplineApp.model.Expense;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record ExpenseResponse(Long expenseId, String expenseType, Float totalValue, Float quantity,
                               Float unitPrice, LocalDateTime createdAt, Long userId) {
@@ -21,12 +22,8 @@ public record ExpenseResponse(Long expenseId, String expenseType, Float totalVal
     }
 
     public static List<ExpenseResponse> fromEntityList(List<Expense> expenseList) {
-        List<ExpenseResponse> expenseResponseList = new ArrayList<>();
-        if (!expenseList.isEmpty()) {
-            for (Expense expense : expenseList) {
-                expenseResponseList.add(fromEntity(expense));
-            }
-        }
-        return expenseResponseList;
+        return expenseList.stream()
+                .map(ExpenseResponse::fromEntity)
+                .collect(Collectors.toList());
     }
 }

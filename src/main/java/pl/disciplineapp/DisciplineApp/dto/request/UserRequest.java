91ab2a281e84bf21e3d7d58b7fff4cbe79ca9ec9
michalgalus <galusmichal@outlook.com>
2026-08-29 14:37:37 +0200

    SCRUM-01 added annotation to UserRequest

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/request/UserRequest.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/request/UserRequest.java
@@ -1,5 +1,7 @@
 package pl.disciplineapp.DisciplineApp.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,9 +9,14 @@ import lombok.Getter;
 @Getter
 public class UserRequest {
     private Long userId;
+    @Size(min = 3)
     private String firstName;
+    @Size(min = 3)
     private String lastName;
+    @Email
+    @Size(min = 5)
     private String email;
+    @Size(min = 5)
     private String password;
     private Boolean enabled;
     private Boolean isSubscriber;

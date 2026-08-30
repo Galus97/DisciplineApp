6cc07b4e29cc21cb4f1a008824d16b36124d48f0
michalgalus <galusmichal@outlook.com>
2026-08-30 22:09:29 +0200

    SCRUM-01 added handleMethodArgumentNotValid to GlobalExceptionHandler

--- src/main/java/pl/disciplineapp/DisciplineApp/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/exception/GlobalExceptionHandler.java
@@ -58,6 +58,23 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST);
+        List<String> mappedErrors = ex.getBindingResult().getAllErrors().stream()
+                .map(this::getErrorMessage)
+                .toList();
+        body.put("errors", mappedErrors);
+        return new ResponseEntity<>(body, headers, status);
+    }
+
     private String getErrorMessage(ObjectError objectError) {
         if (objectError instanceof FieldError) {
             String field = ((FieldError) objectError).getField();

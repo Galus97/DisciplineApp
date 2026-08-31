b937bf8baae6740662592c31c135a6b486d5a805
michalgalus <galusmichal@outlook.com>
2026-08-31 20:34:38 +0200

    SCRUM-01 added toExpenseModel to ExpenseMapper

--- src/main/java/pl/disciplineapp/DisciplineApp/mapper/ExpenseMapper.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/mapper/ExpenseMapper.java
@@ -1,11 +1,17 @@
 package pl.disciplineapp.DisciplineApp.mapper;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import pl.disciplineapp.DisciplineApp.dto.request.ExpenseRequest;
 import pl.disciplineapp.DisciplineApp.dto.response.ExpenseResponse;
 import pl.disciplineapp.DisciplineApp.model.Expense;
+import pl.disciplineapp.DisciplineApp.model.User;
+import pl.disciplineapp.DisciplineApp.service.UserService;
 
 @Component
+@RequiredArgsConstructor
 public class ExpenseMapper {
+    private final UserService userService;
 
     public static ExpenseResponse toExpenseResponse(Expense expense) {
         return new ExpenseResponse(
@@ -18,5 +24,17 @@ public class ExpenseMapper {
                 expense.getCreatedAt(),
                 expense.getUser() != null ? expense.getUser().getUserId() : null
         );
+    }
+
+    public static Expense toExpenseModel(ExpenseRequest expenseRequest, User user) {
+        return Expense.builder()
+                .expenseId(expenseRequest.getExpenseId())
+                .expenseType(expenseRequest.getExpenseType())
+                .expenseDescription(expenseRequest.getExpenseDescription())
+                .totalValue(expenseRequest.getTotalValue())
+                .quantity(expenseRequest.getQuantity())
+                .unitPrice(expenseRequest.getUnitPrice())
+                .user(user)
+                .build();
     }
 }

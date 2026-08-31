8578093253da6349184b705c2ee9107a874351ff
michalgalus <galusmichal@outlook.com>
2026-08-31 17:46:34 +0200

    SCRUM-01 deleted methods from ExpenseResponse

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/response/ExpenseResponse.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/response/ExpenseResponse.java
@@ -1,31 +1,7 @@
 package pl.disciplineapp.DisciplineApp.dto.response;
 
-import pl.disciplineapp.DisciplineApp.model.Expense;
-
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public record ExpenseResponse(Long expenseId, String expenseType, String expenseDescription, Float totalValue,
                               Float quantity, Float unitPrice, LocalDateTime createdAt, Long userId) {
-
-    public static ExpenseResponse fromEntity(Expense expense) {
-        return new ExpenseResponse(
-                expense.getExpenseId(),
-                expense.getExpenseType(),
-                expense.getExpenseDescription(),
-                expense.getTotalValue(),
-                expense.getQuantity(),
-                expense.getUnitPrice(),
-                expense.getCreatedAt(),
-                expense.getUser() != null ? expense.getUser().getUserId() : null
-        );
-    }
-
-    public static List<ExpenseResponse> fromEntityList(List<Expense> expenseList) {
-        return expenseList.stream()
-                .map(ExpenseResponse::fromEntity)
-                .collect(Collectors.toList());
-    }
 }

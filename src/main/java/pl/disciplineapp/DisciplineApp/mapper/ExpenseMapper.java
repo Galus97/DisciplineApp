fb1a21d2ac1bf025fbfa0ee634cfaea8eae346c1
michalgalus <galusmichal@outlook.com>
2026-08-31 17:46:53 +0200

    SCRUM-01 added toExpenseResponse to ExpenseMapper

--- src/main/java/pl/disciplineapp/DisciplineApp/mapper/ExpenseMapper.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/mapper/ExpenseMapper.java
@@ -1,8 +1,22 @@
 package pl.disciplineapp.DisciplineApp.mapper;
 
 import org.springframework.stereotype.Component;
+import pl.disciplineapp.DisciplineApp.dto.response.ExpenseResponse;
+import pl.disciplineapp.DisciplineApp.model.Expense;
 
 @Component
 public class ExpenseMapper {
 
+    public static ExpenseResponse toExpenseResponse(Expense expense) {
+        return new ExpenseResponse(
+                expense.getExpenseId(),
+                expense.getExpenseType(),
+                expense.getExpenseDescription(),
+                expense.getTotalValue(),
+                expense.getQuantity(),
+                expense.getUnitPrice(),
+                expense.getCreatedAt(),
+                expense.getUser() != null ? expense.getUser().getUserId() : null
+        );
+    }
 }

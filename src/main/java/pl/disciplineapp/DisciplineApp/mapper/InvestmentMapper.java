11b6db4f5205d3e83906a1ab5dcd43c743d33dea
michalgalus <galusmichal@outlook.com>
2026-09-01 15:28:17 +0200

    SCRUM-01 added InvestmentMapper

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/mapper/InvestmentMapper.java
@@ -0,0 +1,10 @@
+package pl.disciplineapp.DisciplineApp.mapper;
+
+import org.springframework.stereotype.Component;
+import pl.disciplineapp.DisciplineApp.dto.response.InvestmentResponse;
+import pl.disciplineapp.DisciplineApp.model.Investment;
+
+@Component
+public class InvestmentMapper {
+    
+}

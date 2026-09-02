0f2cf3d058c02e16796d45ba423d2d82796f2932
michalgalus <galusmichal@outlook.com>
2026-09-02 05:14:50 +0200

    SCRUM-01 added User to InvestmentController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/InvestmentController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/InvestmentController.java
@@ -1,10 +1,13 @@
 package pl.disciplineapp.DisciplineApp.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import pl.disciplineapp.DisciplineApp.dto.request.InvestmentRequest;
 import pl.disciplineapp.DisciplineApp.dto.response.InvestmentResponse;
+import pl.disciplineapp.DisciplineApp.model.User;
 import pl.disciplineapp.DisciplineApp.service.InvestmentService;
 
 import java.net.URI;
@@ -17,25 +20,35 @@ public class InvestmentController {
     private final InvestmentService investmentService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<InvestmentResponse> showInvestment(@PathVariable Long id) {
-        return ResponseEntity.ok(investmentService.getInvestment(id));
+    public ResponseEntity<InvestmentResponse> showInvestment(
+            @PathVariable Long id,
+            @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(investmentService.getInvestment(id, user));
     }
 
     @PostMapping
-    public ResponseEntity<InvestmentResponse> saveInvestment(@RequestBody InvestmentRequest investmentRequest) {
-        InvestmentResponse savedInvestment = investmentService.saveInvestment(investmentRequest);
+    public ResponseEntity<InvestmentResponse> saveInvestment(
+            @RequestBody @Valid InvestmentRequest investmentRequest,
+            @AuthenticationPrincipal User user) {
+
+        InvestmentResponse savedInvestment = investmentService.saveInvestment(investmentRequest, user);
+
         return ResponseEntity.created(URI.create("/investment/" + savedInvestment.investmentId()))
                 .body(savedInvestment);
     }
 
     @PutMapping
-    public ResponseEntity<InvestmentResponse> updateInvestment(@RequestBody InvestmentRequest investmentRequest) {
-        return ResponseEntity.ok(investmentService.updateInvestment(investmentRequest));
+    public ResponseEntity<InvestmentResponse> updateInvestment(
+            @RequestBody @Valid InvestmentRequest investmentRequest,
+            @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(investmentService.updateInvestment(investmentRequest, user));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteInvestment(@PathVariable Long id) {
-        investmentService.deleteInvestment(id);
+    public ResponseEntity<Void> deleteInvestment(
+            @PathVariable Long id,
+            @AuthenticationPrincipal User user) {
+        investmentService.deleteInvestment(id, user);
         return ResponseEntity.noContent().build();
     }
 
@@ -43,11 +56,12 @@ public class InvestmentController {
     public ResponseEntity<List<InvestmentResponse>> getInvestments(
             @PathVariable Long userId,
             @RequestParam(required = false) String from,
-            @RequestParam(required = false) String to) {
+            @RequestParam(required = false) String to,
+            @AuthenticationPrincipal User user) {
 
         if (from != null && to != null) {
-            return ResponseEntity.ok(investmentService.getInvestmentsBetweenDates(userId, from, to));
+            return ResponseEntity.ok(investmentService.getInvestmentsBetweenDates(from, to, user));
         }
-        return ResponseEntity.ok(investmentService.getAllInvestment(userId));
+        return ResponseEntity.ok(investmentService.getAllInvestment(user));
     }
 }

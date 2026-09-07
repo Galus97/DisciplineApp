f89e49cdf91cbfbfceef66063f35f248c017b319
michalgalus <galusmichal@outlook.com>
2026-09-07 22:24:50 +0200

    SCRUM-01 added toSavingResponse to SavingMapper

--- src/main/java/pl/disciplineapp/DisciplineApp/mapper/SavingMapper.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/mapper/SavingMapper.java
@@ -1,8 +1,21 @@
 package pl.disciplineapp.DisciplineApp.mapper;
 
 import org.springframework.stereotype.Component;
+import pl.disciplineapp.DisciplineApp.dto.response.SavingResponse;
+import pl.disciplineapp.DisciplineApp.model.Saving;
 
 @Component
 public class SavingMapper {
 
+    public static SavingResponse toSavingResponse(Saving saving) {
+        return new SavingResponse(
+                saving.getSavingId(),
+                saving.getSavingType(),
+                saving.getTotalValue(),
+                saving.getQuantity(),
+                saving.getUnitPrice(),
+                saving.getCreatedAt(),
+                saving.getUser().getUserId()
+        );
+    }
 }

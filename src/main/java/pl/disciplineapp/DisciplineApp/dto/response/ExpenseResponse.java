fda96fda0f180bfb0b0dc7e4f1a352883b703ae5
michalgalus <galusmichal@outlook.com>
2026-09-08 10:38:50 +0200

    SCRUM-01 deleted methods from TaskResponse

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/response/ExpenseResponse.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/response/ExpenseResponse.java
@@ -2,6 +2,13 @@ package pl.disciplineapp.DisciplineApp.dto.response;
 
 import java.time.LocalDateTime;
 
-public record ExpenseResponse(Long expenseId, String expenseType, String expenseDescription, Float totalValue,
-                              Float quantity, Float unitPrice, LocalDateTime createdAt, Long userId) {
+public record ExpenseResponse(
+        Long expenseId,
+        String expenseType,
+        String expenseDescription,
+        Float totalValue,
+        Float quantity,
+        Float unitPrice,
+        LocalDateTime createdAt,
+        Long userId) {
 }

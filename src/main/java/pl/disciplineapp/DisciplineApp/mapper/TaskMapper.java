ca112f15b86ba4a8c8eca0c536cf2a59dbeadf78
michalgalus <galusmichal@outlook.com>
2026-09-08 10:51:03 +0200

    SCRUM-01 added toTaskResponse to TaskMapper

--- src/main/java/pl/disciplineapp/DisciplineApp/mapper/TaskMapper.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/mapper/TaskMapper.java
@@ -6,5 +6,16 @@ import pl.disciplineapp.DisciplineApp.model.Task;
 
 @Component
 public class TaskMapper {
-    
+    public static TaskResponse toTaskResponse(Task task) {
+        return new TaskResponse(
+                task.getTaskId(),
+                task.getTaskName(),
+                task.getDescription(),
+                task.isCompleted(),
+                task.getCreatedAt(),
+                task.getCompletedAt(),
+                task.getDeadline(),
+                task.getUser().getUserId()
+        );
+    }
 }

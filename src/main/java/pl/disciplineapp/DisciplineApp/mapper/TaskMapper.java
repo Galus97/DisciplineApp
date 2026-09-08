25ba20e53774926e3a5e8421b4a03f068c5ae101
michalgalus <galusmichal@outlook.com>
2026-09-08 10:54:36 +0200

    SCRUM-01 added toTaskModel to TaskMapper

--- src/main/java/pl/disciplineapp/DisciplineApp/mapper/TaskMapper.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/mapper/TaskMapper.java
@@ -1,8 +1,10 @@
 package pl.disciplineapp.DisciplineApp.mapper;
 
 import org.springframework.stereotype.Component;
+import pl.disciplineapp.DisciplineApp.dto.request.TaskRequest;
 import pl.disciplineapp.DisciplineApp.dto.response.TaskResponse;
 import pl.disciplineapp.DisciplineApp.model.Task;
+import pl.disciplineapp.DisciplineApp.model.User;
 
 @Component
 public class TaskMapper {
@@ -17,5 +19,18 @@ public class TaskMapper {
                 task.getDeadline(),
                 task.getUser().getUserId()
         );
+    }
+
+    public static Task toTaskModel(TaskRequest taskRequest, User user) {
+        return Task.builder()
+                .taskId(taskRequest.getTaskId())
+                .taskName(taskRequest.getTaskName())
+                .description(taskRequest.getDescription())
+                .completed(taskRequest.isCompleted())
+                .createdAt(taskRequest.getCreatedAt())
+                .completedAt(taskRequest.getCompletedAt())
+                .deadline(taskRequest.getDeadline())
+                .user(user)
+                .build();
     }
 }

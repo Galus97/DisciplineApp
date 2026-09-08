6c8e4c35e31526ce331d5be007c4f24a9d8e289a
michalgalus <galusmichal@outlook.com>
2026-09-08 10:36:58 +0200

    SCRUM-01 deleted SavingResponse

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/response/SavingResponse.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/response/SavingResponse.java
@@ -1,29 +1,13 @@
 package pl.disciplineapp.DisciplineApp.dto.response;
 
-import pl.disciplineapp.DisciplineApp.model.Saving;
-
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
-public record SavingResponse(Long savingId, String savingType, Float TotalValue,
-                             Float quantity, Float unitPrice, LocalDateTime createdAt, Long userId) {
-    public static SavingResponse fromEntity(Saving saving) {
-        return new SavingResponse(
-                saving.getSavingId(),
-                saving.getSavingType(),
-                saving.getTotalValue(),
-                saving.getQuantity(),
-                saving.getUnitPrice(),
-                saving.getCreatedAt(),
-                saving.getUser() != null ? saving.getUser().getUserId() : null
-        );
-    }
-
-    public static List<SavingResponse> fromEntityList(List<Saving> savingList) {
-        return savingList.stream()
-                .map(SavingResponse::fromEntity)
-                .collect(Collectors.toList());
-    }
+public record SavingResponse(
+        Long savingId,
+        String savingType,
+        Float TotalValue,
+        Float quantity,
+        Float unitPrice,
+        LocalDateTime createdAt,
+        Long userId) {
 }

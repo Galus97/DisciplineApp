fd2641203dbe6967a901e3c32f9ee7b448247bd2
michalgalus <galusmichal@outlook.com>
2026-09-08 10:50:48 +0200

    SCRUM-01 added TaskMapper

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/mapper/TaskMapper.java
@@ -0,0 +1,10 @@
+package pl.disciplineapp.DisciplineApp.mapper;
+
+import org.springframework.stereotype.Component;
+import pl.disciplineapp.DisciplineApp.dto.response.TaskResponse;
+import pl.disciplineapp.DisciplineApp.model.Task;
+
+@Component
+public class TaskMapper {
+    
+}

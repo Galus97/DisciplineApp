855f6f335617730cd2d337251e06075a897755a6
michalgalus <galusmichal@outlook.com>
2026-09-08 11:12:49 +0200

    SCRUM-01 added toUserResponse to UserMapper

--- src/main/java/pl/disciplineapp/DisciplineApp/mapper/UserMapper.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/mapper/UserMapper.java
@@ -1,4 +1,18 @@
 package pl.disciplineapp.DisciplineApp.mapper;
 
+import pl.disciplineapp.DisciplineApp.dto.response.UserResponse;
+import pl.disciplineapp.DisciplineApp.model.User;
+
 public class UserMapper {
+    public static UserResponse toUserResponse(User user) {
+        return new UserResponse(
+                user.getUserId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getEnabled(),
+                user.getIsSubscriber()
+        );
+    }
 }

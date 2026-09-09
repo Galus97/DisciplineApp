0905fd7180a3b4059ac1571fc7e45738f07e1895
michalgalus <galusmichal@outlook.com>
2026-09-09 12:17:10 +0200

    SCRUM-01 added saveNewUser to RegistrationUserService

--- src/main/java/pl/disciplineapp/DisciplineApp/service/auth/RegistrationUserService.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/service/auth/RegistrationUserService.java
@@ -2,8 +2,33 @@ package pl.disciplineapp.DisciplineApp.service.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.disciplineapp.DisciplineApp.component.ErrorMessages;
+import pl.disciplineapp.DisciplineApp.dto.request.UserRegistrationRequest;
+import pl.disciplineapp.DisciplineApp.dto.response.UserResponse;
+import pl.disciplineapp.DisciplineApp.exception.ValidationException;
+import pl.disciplineapp.DisciplineApp.mapper.UserMapper;
+import pl.disciplineapp.DisciplineApp.model.User;
+import pl.disciplineapp.DisciplineApp.repository.UserRepository;
+import pl.disciplineapp.DisciplineApp.util.RegisterValidator;
+import pl.disciplineapp.DisciplineApp.util.ServiceValidator;
 
 @Service
 @RequiredArgsConstructor
 public class RegistrationUserService {
+    private final UserRepository userRepository;
+    private final RegisterValidator registerValidator;
+    private final ServiceValidator serviceValidator;
+
+    @Transactional
+    public UserResponse saveNewUser(UserRegistrationRequest userRequest) throws ValidationException {
+        serviceValidator.throwIfRequestIsNull(userRequest, ErrorMessages.USER_REQUEST_IS_NULL);
+
+        if(registerValidator.validateUser(userRequest).isEmpty()){
+            User user = UserMapper.toUserModel(userRequest);
+            return UserMapper.toUserResponse(userRepository.save(user));
+        } else {
+            throw new ValidationException(registerValidator.validateUser(userRequest));
+        }
+    }
 }

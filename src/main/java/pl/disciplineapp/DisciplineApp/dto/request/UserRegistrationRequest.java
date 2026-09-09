af8d15ffdeeffd2e2f46a6604eacc10d9e2829d4
michalgalus <galusmichal@outlook.com>
2026-09-09 11:37:02 +0200

    SCRUM-01 added UserRegistrationRequest

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/request/UserRegistrationRequest.java
@@ -0,0 +1,22 @@
+package pl.disciplineapp.DisciplineApp.dto.request;
+
+import jakarta.validation.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+
+public class UserRegistrationRequest {
+    @Length(min = 3)
+    private String firstName;
+
+    @Length(min = 3)
+    private String lastName;
+
+    @Length(min = 5)
+    @Email
+    private String email;
+
+    @Length(min = 5)
+    private String password;
+
+    @Length(min = 5)
+    private String repeatPassword;
+}

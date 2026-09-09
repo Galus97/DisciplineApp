6ef118279d57fc746054d095ba830c4677f0ab99
michalgalus <galusmichal@outlook.com>
2026-09-09 14:47:00 +0200

    SCRUM-01 added findByIdAndUser to TaskRepository

--- src/main/java/pl/disciplineapp/DisciplineApp/dto/request/TaskRequest.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/dto/request/TaskRequest.java
@@ -1,11 +1,8 @@
 package pl.disciplineapp.DisciplineApp.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Builder
@@ -13,13 +10,12 @@ public class TaskRequest {
     private Long taskId;
     @NotBlank
     private String taskName;
-    @NotBlank
     private String description;
     private boolean completed;
-    // TODO Those LocalDateTimes should be here?
-    private LocalDateTime createdAt;
-    private LocalDateTime completedAt;
-    private LocalDateTime deadline;
-    @NotNull
-    private Long userId;
+    @NotBlank
+    private String createdAt;
+    @NotBlank
+    private String completedAt;
+    @NotBlank
+    private String deadline;
 }
--- src/main/java/pl/disciplineapp/DisciplineApp/repository/TaskRepository.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/repository/TaskRepository.java
@@ -1,21 +1,26 @@
 package pl.disciplineapp.DisciplineApp.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import pl.disciplineapp.DisciplineApp.model.Task;
+import pl.disciplineapp.DisciplineApp.model.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    List<Task> findAllByUser_UserId(Long userId);
+    List<Task> findAllByUser(User user, Pageable pageable);
 
-    @Query("SELECT t FROM Task t WHERE t.user.userId = :userId AND t.createdAt BETWEEN :from AND :to")
-    List<Task> findAllByUserIdAndCreatedAtBetween(
-            @Param("userId") Long userId,
+    Optional<Task> findByIdAndUser(Long id, User user);
+
+    @Query("SELECT t FROM Task t WHERE t.user= :user AND t.createdAt BETWEEN :from AND :to")
+    List<Task> findAllByUserAndCreatedAtBetween(
+            @Param("user") User user,
             @Param("from") LocalDateTime from,
-            @Param("to") LocalDateTime to
-            );
+            @Param("to") LocalDateTime to,
+            Pageable pageable);
 }

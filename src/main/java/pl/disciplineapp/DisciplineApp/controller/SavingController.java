e5d207920762873c219a25c63253b02406c09443
michalgalus <galusmichal@outlook.com>
2026-09-09 13:10:51 +0200

    SCRUM-01 changed methods in SavingController to use User

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/SavingController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/SavingController.java
@@ -1,10 +1,14 @@
 package pl.disciplineapp.DisciplineApp.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import pl.disciplineapp.DisciplineApp.dto.request.SavingRequest;
 import pl.disciplineapp.DisciplineApp.dto.response.SavingResponse;
+import pl.disciplineapp.DisciplineApp.model.User;
 import pl.disciplineapp.DisciplineApp.service.SavingService;
 
 import java.net.URI;
@@ -17,24 +21,32 @@ public class SavingController {
     private final SavingService savingService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<SavingResponse> showSaving(@PathVariable Long id) {
-        return ResponseEntity.ok(savingService.getSavingResponse(id));
+    public ResponseEntity<SavingResponse> showSaving(
+            @PathVariable Long id,
+            @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(savingService.getSavingResponse(id, user));
     }
 
     @PostMapping
-    public ResponseEntity<SavingResponse> createSaving(@RequestBody SavingRequest savingRequest) {
-        SavingResponse savedSaving = savingService.saveSaving(savingRequest);
+    public ResponseEntity<SavingResponse> createSaving(
+            @RequestBody @Valid SavingRequest savingRequest,
+            @AuthenticationPrincipal User user) {
+        SavingResponse savedSaving = savingService.saveSaving(savingRequest, user);
         return ResponseEntity.created(URI.create("/saving/" + savedSaving.savingId())).body(savedSaving);
     }
 
     @PutMapping
-    public ResponseEntity<SavingResponse> updateSaving(@RequestBody SavingRequest savingRequest) {
-        return ResponseEntity.ok(savingService.updateSaving(savingRequest));
+    public ResponseEntity<SavingResponse> updateSaving(
+            @RequestBody @Valid SavingRequest savingRequest,
+            @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(savingService.updateSaving(savingRequest, user));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteSaving(@PathVariable Long id) {
-        savingService.deleteSaving(id);
+    public ResponseEntity<Void> deleteSaving(
+            @PathVariable Long id,
+            @AuthenticationPrincipal User user) {
+        savingService.deleteSaving(id, user);
         return ResponseEntity.noContent().build();
     }
 
@@ -42,10 +54,12 @@ public class SavingController {
     public ResponseEntity<List<SavingResponse>> getSavings(
             @PathVariable Long userId,
             @RequestParam(required = false) String from,
-            @RequestParam(required = false) String to) {
+            @RequestParam(required = false) String to,
+            @AuthenticationPrincipal User user,
+            Pageable pageable) {
         if (from != null && to != null) {
-            return ResponseEntity.ok(savingService.getSavingBetweenDates(userId, from, to));
+            return ResponseEntity.ok(savingService.getSavingBetweenDates(user, from, to, pageable));
         }
-        return ResponseEntity.ok(savingService.getAllSaving(userId));
+        return ResponseEntity.ok(savingService.getAllSaving(user, pageable));
     }
 }

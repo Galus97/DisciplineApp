ca202d9b4377c110b9d3cc58396d706c976f7749
michalgalus <galusmichal@outlook.com>
2026-09-09 11:46:36 +0200

    SCRUM-01 updated SpringSecurity

--- src/main/java/pl/disciplineapp/DisciplineApp/configuration/SpringSecurity.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/configuration/SpringSecurity.java
@@ -1,31 +1,37 @@
 package pl.disciplineapp.DisciplineApp.configuration;
 
-import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableMethodSecurity
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SpringSecurity {
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http)  throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize
-                .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-        );
-        http
+        return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((request) -> request
-                        .requestMatchers("/", "/CSS/**", "/js/**", "/images/**", "/webjars/**").permitAll()
-                        .anyRequest().permitAll()
-                );
-        return http.build();
+                        .requestMatchers("/auth/**", "/error", "/CSS/**", "/js/**", "/images/**", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .userDetailsService(userDetailsService)
+                .build();
     }
 
     @Bean

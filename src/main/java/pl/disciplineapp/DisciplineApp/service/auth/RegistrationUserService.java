d8ce6cf792d05c1cc041e428d0001c2ea12735cc
michalgalus <galusmichal@outlook.com>
2026-09-09 12:11:30 +0200

    SCRUM-01 added RegistrationUserService

---
+++ src/main/java/pl/disciplineapp/DisciplineApp/service/auth/RegistrationUserService.java
@@ -0,0 +1,9 @@
+package pl.disciplineapp.DisciplineApp.service.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RegistrationUserService {
+}

edbb50c7217c1884646667a3aaf0159560a9bac2
michalgalus <galusmichal@outlook.com>
2026-09-09 12:20:29 +0200

    SCRUM-01 added register to AuthController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/AuthController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/AuthController.java
@@ -10,18 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.disciplineapp.DisciplineApp.dto.request.UserLoginRequest;
 import pl.disciplineapp.DisciplineApp.dto.request.UserRegistrationRequest;
 import pl.disciplineapp.DisciplineApp.dto.response.UserResponse;
-import pl.disciplineapp.DisciplineApp.service.UserService;
+import pl.disciplineapp.DisciplineApp.exception.ValidationException;
+import pl.disciplineapp.DisciplineApp.service.auth.RegistrationUserService;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
-    private final UserService userService;
+    private final RegistrationUserService registrationService;
 
     @PostMapping("/login")
     public ResponseEntity<UserResponse> login(@RequestBody @Valid UserLoginRequest request) {
         return ResponseEntity.ok().build();
     }
 
-    
+    @PostMapping("/registration")
+    public ResponseEntity<UserResponse> register(@RequestBody @Valid UserRegistrationRequest request) throws ValidationException {
+        UserResponse userResponse = registrationService.saveNewUser(request);
+        return ResponseEntity
+                .created(URI.create("/auth/registration/" + userResponse.userId()))
+                .body(userResponse);
+    }
 }

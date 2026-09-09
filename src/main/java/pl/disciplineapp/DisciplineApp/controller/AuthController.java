537425ffd054c957f29f2f15a5055ae658e40fb0
michalgalus <galusmichal@outlook.com>
2026-09-09 11:53:26 +0200

    SCRUM-01 added login to AuthController

--- src/main/java/pl/disciplineapp/DisciplineApp/controller/AuthController.java
+++ src/main/java/pl/disciplineapp/DisciplineApp/controller/AuthController.java
@@ -1,8 +1,15 @@
 package pl.disciplineapp.DisciplineApp.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.disciplineapp.DisciplineApp.dto.request.UserLoginRequest;
+import pl.disciplineapp.DisciplineApp.dto.request.UserRegistrationRequest;
+import pl.disciplineapp.DisciplineApp.dto.response.UserResponse;
 import pl.disciplineapp.DisciplineApp.service.UserService;
 
 @RestController
@@ -10,4 +17,11 @@ import pl.disciplineapp.DisciplineApp.service.UserService;
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponse> login(@RequestBody @Valid UserLoginRequest request) {
+        return ResponseEntity.ok().build();
+    }
+
+    
 }
